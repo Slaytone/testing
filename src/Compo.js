51bfd7112ac4636458d0cb8aa1d7014df710af9a
slaytone <you@examle.com>
2019-10-10 17:38:04 +0300

    add file

---
+++ src/Compo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Compo() {
+  return (
+    <div className="main-block">
+      <h1 className="main-block-header">	The Main block    </h1>
+	  <p> description block </p>
+    </div>
+  );
+}
+
+export default Compo;
